27d6f1ed76a31eb1817347cd1f2b4625b78509fb
Prada Hsiung <bear.prada@gmail.com>
2018-05-14 08:15:22 +0800

    replace camera library to cwac-camera

--- app/src/main/java/plantscam/android/prada/lab/plantscamera/CameraActivity.kt
+++ app/src/main/java/plantscam/android/prada/lab/plantscamera/CameraActivity.kt
@@ -1,10 +1,14 @@
 package plantscam.android.prada.lab.plantscamera
 
 import android.Manifest
+import android.content.Context
+import android.hardware.Camera
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import com.google.android.cameraview.CameraView
+import com.commonsware.cwac.camera.CameraHost
+import com.commonsware.cwac.camera.CameraHostProvider
+import com.commonsware.cwac.camera.SimpleCameraHost
 import com.tbruyelle.rxpermissions2.RxPermissions
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -13,21 +17,23 @@ import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
 import kotlinx.android.synthetic.main.activity_camera.*
 
-class CameraActivity : AppCompatActivity() {
+
+class CameraActivity : AppCompatActivity(), CameraHostProvider {
 
     private lateinit var cameraViewModel : CameraViewModel
     val cameraBuffSignal: Subject<CameraPreviewData> = BehaviorSubject.create()
     val takePhotoSignal: Subject<ByteArray> = PublishSubject.create()
+    private var myHost: MyHost? = null
 
     private val disposeBag = CompositeDisposable()
-    private val cameraCallback = object: CameraView.Callback() {
-        override fun onPreviewFrame(cameraView: CameraView?, data: ByteArray?, width: Int, height: Int, format: Int) {
-            data?.let { cameraBuffSignal.onNext(CameraPreviewData(it, width, height)) }
-        }
-        override fun onPictureTaken(cameraView: CameraView?, jpegData: ByteArray?) {
-            jpegData?.let { takePhotoSignal.onNext(it) }
-        }
-    }
+//    private val cameraCallback = object: CameraView.Callback() {
+//        override fun onPreviewFrame(cameraView: CameraView?, data: ByteArray?, width: Int, height: Int, format: Int) {
+//            data?.let { cameraBuffSignal.onNext(CameraPreviewData(it, width, height)) }
+//        }
+//        override fun onPictureTaken(cameraView: CameraView?, jpegData: ByteArray?) {
+//            jpegData?.let { takePhotoSignal.onNext(it) }
+//        }
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,14 +41,17 @@ class CameraActivity : AppCompatActivity() {
 
         cameraViewModel = CameraViewModel(assets)
 
-        camera.addCallback(cameraCallback)
+        camera.setPreviewCallback { data, camera ->
+            val size = camera.parameters.previewSize
+            cameraBuffSignal.onNext(CameraPreviewData(data, size.width, size.height))
+        }
         btn_take_photo.setOnClickListener {
             val rxPermissions = RxPermissions(this)
             disposeBag.add(rxPermissions
                 .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 .subscribe({ granted ->
                     if (granted) {
-                        camera.takePicture()
+                        camera.takePicture(false, true)
                     } else {
                         Toast.makeText(baseContext, "permission denied!!", Toast.LENGTH_LONG).show()
                     }
@@ -52,7 +61,8 @@ class CameraActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        camera.start()
+        camera.onResume()
+        camera.restartPreview()
 
         cameraViewModel.bindIntents(cameraBuffSignal, takePhotoSignal)
 
@@ -64,10 +74,17 @@ class CameraActivity : AppCompatActivity() {
     }
 
     override fun onPause() {
-        camera.stop()
+        camera.onPause()
         super.onPause()
         cameraViewModel.unbindIntents()
         disposeBag.clear()
+    }
+
+    override fun getCameraHost(): CameraHost {
+        if (myHost == null) {
+            myHost = MyHost(this)
+        }
+        return myHost!!
     }
 
     data class CameraPreviewData(
@@ -75,4 +92,47 @@ class CameraActivity : AppCompatActivity() {
         val width: Int,
         val height: Int
     )
+
+    // add more configuration later
+    private class MyHost(ctx: Context) : SimpleCameraHost(ctx) {
+        var _cameraId : Int = -1
+
+        override fun getCameraId(): Int {
+            if (_cameraId == -1) {
+                initCameraId()
+            }
+            return _cameraId
+        }
+
+        override fun getPreviewSize(displayOrientation: Int,
+                                    width: Int,
+                                    height: Int,
+                                    parameters: Camera.Parameters): Camera.Size? {
+            previewSize = getOptimalPreviewSize(parameters.supportedPreviewSizes, width / 2, height / 2)
+            return previewSize
+        }
+
+        private fun initCameraId() {
+            val count = Camera.getNumberOfCameras()
+            var result = -1
+            if (count > 0) {
+                result = 0 // if we have a camera, default to this one
+                val info = Camera.CameraInfo()
+                for (i in 0 until count) {
+                    Camera.getCameraInfo(i, info)
+                    if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                        result = i
+                        break
+                    }
+                }
+            }
+            _cameraId = result
+        }
+
+        override fun getRecordingHint(): CameraHost.RecordingHint {
+            return CameraHost.RecordingHint.NONE
+        }
+    }
+
+
 }
--- app/src/main/java/plantscam/android/prada/lab/plantscamera/MainActivity.kt
+++ app/src/main/java/plantscam/android/prada/lab/plantscamera/MainActivity.kt
@@ -1,10 +1,13 @@
 package plantscam.android.prada.lab.plantscamera
 
+import android.Manifest.permission.CAMERA
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.tbruyelle.rxpermissions2.RxPermissions
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -14,41 +17,41 @@ import plantscam.android.prada.lab.plantscamera.ml.PlantFreezeClassifier2
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var classifier: PlantFreezeClassifier2
+//    private lateinit var classifier: PlantFreezeClassifier2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        classifier = PlantFreezeClassifier2(assets)
+//        classifier = PlantFreezeClassifier2(assets)
 
         btn_select_photo.setOnClickListener {
-//            val rxPermissions = RxPermissions(this)
-//            rxPermissions
-//                .request(CAMERA)
-//                .subscribe({ granted ->
-//                    if (granted) {
-//                        startActivity(Intent(this, CameraActivity::class.java))
-//                    } else {
-//                        Toast.makeText(this, "permission denied!!", Toast.LENGTH_LONG).show()
-//                    }
-//                })
+            val rxPermissions = RxPermissions(this)
+            rxPermissions
+                .request(CAMERA)
+                .subscribe({ granted ->
+                    if (granted) {
+                        startActivity(Intent(this, CameraActivity::class.java))
+                    } else {
+                        Toast.makeText(this, "permission denied!!", Toast.LENGTH_LONG).show()
+                    }
+                })
 
-             //  Test TensorFlow
-            Observable.fromCallable {
-                val bm = BitmapFactory.decodeStream(assets.open("data/3.jpg"))
-                Bitmap.createScaledBitmap(bm,
-                    PlantFreezeClassifier2.INPUT_W,
-                    PlantFreezeClassifier2.INPUT_H, false)
-            }
-            .map { PlantFreezeClassifier2.convertTo(it) }
-            // .map { classifier.run(it, fakeLenPixel, fakeLenCm) }
-            .map { classifier.run(it) }
-            .subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                Toast.makeText(this, "detected result = " + it, Toast.LENGTH_LONG).show()
-            }
+//             //  Test TensorFlow
+//            Observable.fromCallable {
+//                val bm = BitmapFactory.decodeStream(assets.open("data/3.jpg"))
+//                Bitmap.createScaledBitmap(bm,
+//                    PlantFreezeClassifier2.INPUT_W,
+//                    PlantFreezeClassifier2.INPUT_H, false)
+//            }
+//            .map { PlantFreezeClassifier2.convertTo(it) }
+//            // .map { classifier.run(it, fakeLenPixel, fakeLenCm) }
+//            .map { classifier.run(it) }
+//            .subscribeOn(Schedulers.computation())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe {
+//                Toast.makeText(this, "detected result = " + it, Toast.LENGTH_LONG).show()
+//            }
         }
     }
 
